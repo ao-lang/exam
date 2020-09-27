0e91a5f585550a5f3470f3d1dbe7e16a7febe3fb
MrDragon12321 <71914630+MrDragon12321@users.noreply.github.com>
2020-09-27 22:29:37 +0800

    跳

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,12 +2,16 @@
   <div class="jumbotron mt-3 py-3">
     <!-- 题目 -->
     <h1 class="display-4">
-      <span class="badge badge-dark mr-3">{{currentQuestion.id}}</span>
-      {{currentQuestion.title}}
+      <span class="badge badge-dark mr-3">{{ currentQuestion.id }}</span>
+      {{ currentQuestion.title }}
     </h1>
     <hr class="my-2" />
     <!-- 选项 -->
-    <div v-for="(item, index) in currentQuestion.items" :key="index" class="form-check my-3">
+    <div
+      v-for="(item, index) in currentQuestion.items"
+      :key="index"
+      class="form-check my-3"
+    >
       <label class="form-check-label">
         <input
           @change="setAnswer(currentQuestion.id)"
@@ -17,15 +21,19 @@
           name="item"
           :value="itemIndexs[index]"
         />
-        <span class="badge badge-pill badge-danger mr-2">{{itemIndexs[index]}}</span>
-        {{item}}
+        <span class="badge badge-pill badge-danger mr-2">{{
+          itemIndexs[index]
+        }}</span>
+        {{ item }}
       </label>
     </div>
     <!-- 按钮 -->
     <div class="row">
       <div class="col-lg-6 col-sm-12">
         <!-- 按钮是否可用 -->
-        <button @click="prev" type="button" class="btn btn-danger btn-block">上一题</button>
+        <button @click="prev" type="button" class="btn btn-danger btn-block">
+          上一题
+        </button>
       </div>
       <div class="col-lg-6 col-sm-12">
         <button
@@ -33,14 +41,34 @@
           @click="submit"
           type="button"
           class="btn btn-primary btn-block mt-sm-3 mt-lg-0"
-        >完成</button>
+        >
+          完成
+        </button>
         <button
           v-else
           @click="next"
           type="button"
           class="btn btn-success btn-block mt-sm-3 mt-lg-0"
-        >下一题</button>
+        >
+          下一题
+        </button>
       </div>
+    </div>
+    <div class="mt-4">
+    <span v-for="(item, index) in list" :key="index" >
+      <button
+        @click="skip(index)"
+        v-if="a.has(index + 1)"
+        type="button"
+        class="btn btn-primary"
+      >
+        {{ index + 1 }}
+      </button>
+
+      <button @click="skip(index)" v-else type="button" class="btn btn-info">
+        {{ index + 1 }}
+      </button>
+    </span>
     </div>
   </div>
 </template>
@@ -56,33 +84,37 @@ export default {
       answer: undefined,
       itemIndexs: ["A", "B", "C", "D", "E"],
       current: 0,
-      a:new Map()
+      a: new Map(),
     };
   },
   methods: {
+    skip: function (i) {
+      this.current = i;
+      this.answer = this.a.get(this.current+1);
+    },
     setAnswer: function (id) {
       // 设置答案
       // console.log(`${id}:${this.answer}`);
       // 当前题目的答案传递到父组件
       // 触发一个自定义事件，并事件监听器传递参数
       this.$emit("on-answer", id, this.answer);
-      this.a.set(id,this.answer)
+      this.a.set(id, this.answer);
     },
     next: function () {
       if (this.current < this.list.length - 1) {
-         this.current++;
+        this.current++;
         if (this.a.has(this.current)) {
-                        this.answer = this.a.get(this.current + 1);
-                    } else {
-                        this.answer = '';
-                    }
+          this.answer = this.a.get(this.current + 1);
+        } else {
+          this.answer = "";
+        }
       }
     },
     prev: function () {
-      let that=this
+      let that = this;
       if (this.current > 0) {
         this.current--;
-        this.answer = this.a.get(this.current+1);        
+        this.answer = this.a.get(this.current + 1);
       }
     },
     submit: function () {
